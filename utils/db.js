5587485a2d585d38da9043a6e45d404d8bb2ea80
okodee <okodee@example.com>
2025-05-15 08:03:45 -0400

    first commit

--- utils/db.js
+++ utils/db.js
@@ -4,34 +4,27 @@ const connection = {};
 
 async function connect() {
   if (connection.isConnected) {
-    console.log('already connected');
     return;
   }
+
   if (mongoose.connections.length > 0) {
     connection.isConnected = mongoose.connections[0].readyState;
+
     if (connection.isConnected === 1) {
-      console.log('use previous connection');
       return;
     }
+
     await mongoose.disconnect();
   }
-  const db = await mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  });
-  console.log('new connection');
+
+  const db = await mongoose.connect(process.env.MONGODB_URI);
   connection.isConnected = db.connections[0].readyState;
 }
 
 async function disconnect() {
-  if (connection.isConnected) {
-    if (process.env.NODE_ENV === 'production') {
-      await mongoose.disconnect();
-      connection.isConnected = false;
-    } else {
-      console.log('not disconnected');
-    }
+  if (process.env.NODE_ENV === 'production') {
+    await mongoose.disconnect();
+    connection.isConnected = false;
   }
 }
 
@@ -42,5 +35,4 @@ function convertDocToObj(doc) {
   return doc;
 }
 
-const db = { connect, disconnect, convertDocToObj };
-export default db;
+export default { connect, disconnect, convertDocToObj };
